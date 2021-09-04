1478fa0aeefdb80b02b64080080b8a050f128260
Christofer Paglinawan <christoferjohn.paglinawan@benilde.edu.ph>
2021-09-04 12:41:44 +0800

    40% done with module 1, will continue later

--- Module 0/src/com/christoferpaglinawan/Main.java
+++ Module 0/src/com/christoferpaglinawan/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] numbers = new int [5];
+        float[] numbers = new float [20];
 
         numbers[0] = 90;
         numbers[1] = 85;
