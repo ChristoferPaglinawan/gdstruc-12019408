f4887acd02bc9b318ed9cdbda569bca891a1a2db
Christofer Paglinawan <christoferjohn.paglinawan@benilde.edu.ph>
2021-09-05 22:23:50 +0800

    finished number 1, still yet to finish number 2

--- gdstruc-module1-sroting/src/com/gdstruc/module1/Main.java
+++ gdstruc-module1-sroting/src/com/gdstruc/module1/Main.java
@@ -17,12 +17,12 @@ public class Main {
         numbers[8] = 26;
         numbers[9] = 13;
 
-        System.out.println("Before Bubble Sort:");
+        System.out.println("Before Selection Sort:");
         printArrayElements(numbers);
 
-        bubbleSort(numbers);
+        selectionSort(numbers);
 
-        System.out.println("\n\nAfter Bubble Sort:");
+        System.out.println("\n\nAfter Selection Sort:");
         printArrayElements(numbers);
     }
 
@@ -32,13 +32,33 @@ public class Main {
         {
            for (int i = 0; i < lastSortedIndex; i++)
            {
-               if (arr[i] > arr[i + 1])
+               if (arr[i] < arr[i + 1])
                {
                    int temp = arr[i];
                    arr[i] = arr[i + 1];
                    arr[i + 1] = temp;
                }
            }
+        }
+    }
+
+    private static void selectionSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            int largestIndex = 0;
+
+            for (int i = 1; i <= lastSortedIndex; i++)
+            {
+                if (arr[i] < arr[largestIndex])
+                {
+                    largestIndex = i;
+                }
+            }
+
+            int temp = arr[lastSortedIndex];
+            arr[lastSortedIndex] = arr[largestIndex];
+            arr[largestIndex] = temp;
         }
     }
 
